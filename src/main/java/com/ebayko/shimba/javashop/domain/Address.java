d47fcd1aa7a6ba58287969ec7cef1a9b9390643d
guma <martillo8@nate.com>
2018-06-10 20:05:36 +0900

    order address

---
+++ src/main/java/com/ebayko/shimba/javashop/domain/Address.java
@@ -0,0 +1,28 @@
+package com.ebayko.shimba.javashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+public class Address implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "address_id")
+    private Long addressId;
+    private String name;
+    private String phoneNo;
+    private String address1;
+    private String address2;
+
+    @Column(name = "reg_date")
+    private LocalDateTime regDate;
+}
---
+++ src/main/java/com/ebayko/shimba/javashop/domain/Order.java
@@ -0,0 +1,38 @@
+package com.ebayko.shimba.javashop.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "cart")
+@Getter
+@Setter
+@AllArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long orderId;
+
+    @ManyToOne(targetEntity = Item.class)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne(targetEntity = User.class)
+    @JoinColumn(name = "buyer_id")
+    private User buyer;
+    private Integer itemQty;
+
+    @ManyToOne(targetEntity = Address.class)
+    @JoinColumn(name = "address_id")
+    private Address shippingAddress;
+
+    @Column(name = "reg_date")
+    private LocalDateTime regDate;
+    @Column(name = "chg_date")
+    private LocalDateTime chgDate;
+}
